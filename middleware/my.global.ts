6c71f21bfacc2315e65196c60006c9ce0797b216
Edge <edge27452696803@gmail.com>
2024-07-25 00:10:08 +0800

    piain

--- middleware/my.global.ts
+++ middleware/my.global.ts
@@ -1,6 +1,3 @@
 export default defineNuxtRouteMiddleware((to) => { 
     console.log(to.path);
-    if (to.path !== '/user/123') {
-        return navigateTo('/user/123');
-    }
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>index page</div>
+    <div>
+        <button @click="countStore.addCount">add</button>
+        <button @click="countStore.subCount">subtract</button>
+        <p>{{ countStore.count }}</p>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { useCounterStore } from '~/stores/counter';
+const countStore = useCounterStore();
+</script>
