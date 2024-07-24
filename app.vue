b61382da3ccce511c254847eb1b96faed6abbf52
Edge <edge27452696803@gmail.com>
2024-07-24 23:16:38 +0800

    111

--- app.vue
+++ app.vue
@@ -1,16 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-console.log('app.vue');
-const count = ref(0);
-const count2 = ref(0);
-if (count.value === 0) {
-  count.value = 1;
-}
 </script>
---
+++ pages/user.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        user
+        <NuxtPage/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+const route = useRoute();
+onMounted(() => {
+    if (route.path === '/user') {
+      navigateTo('/user/123');
+    }
+});
+</script>
+`
